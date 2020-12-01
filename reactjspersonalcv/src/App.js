e32c1f55fe6af1f161f3835ce08da9ea58cb8944
Rosca Diana <cristirock69@gmail.com>
2020-12-01 17:03:40 +0200

    add

--- reactjspersonalcv/src/App.js
+++ reactjspersonalcv/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Personal CV
+    <div className="Header">
+      Home
     </div>
   );
 }
