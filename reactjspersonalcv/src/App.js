cb415df4711bffd56f09f5de5c48226cf0981990
Rosca Diana <cristirock69@gmail.com>
2020-12-02 13:02:47 +0200

    main page

--- reactjspersonalcv/src/App.js
+++ reactjspersonalcv/src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import React from "react";
 import Header from "./components/header/header.jsx";
 import Navbar from "./components/navbar/navbar.jsx";
-import logo from "./components/header/logo.jpg";
 
 class App extends React.Component {
   render() {
     return (
       <div>
       <Header></Header>
-      <Navbar></Navbar>
       <div className="some-info"> Hi, I'm Cristian Sava and I'm a future Front-End Developer</div>
-      <img src={logo} alt="myself"></img>
+      <Navbar></Navbar>
       </div>
     )
   }
